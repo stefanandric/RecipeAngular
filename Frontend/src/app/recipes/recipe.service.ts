f4c6b274dea43c99582230e3e0a1930a71ca7ebc
Stefan Andric <stefan.andric9@hotmail.com>
2019-12-23 19:36:26 +0100

    Replace Promises with Observable

--- Frontend/src/app/recipes/recipe.service.ts
+++ Frontend/src/app/recipes/recipe.service.ts
@@ -1,82 +1,78 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 
 import { Recipe } from './recipe';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './../shopping-list/shopping-list.service';
 import { DatabaseDataService } from '../shared/database-data.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Injectable()
-export class RecipeService {
+export class RecipeService implements OnDestroy {
 
-      constructor(private slService: ShoppingListService,
-                  private dataProvider: DatabaseDataService) {}
+  constructor(private slService: ShoppingListService,
+              private dataProvider: DatabaseDataService) {}
 
   recipesChanged = new Subject<Recipe[]>();
 
-  // private recipes: Recipe[] = [
-  //       new Recipe('Tasty Schnitzle', 'A super lazy-tasty Schnitzle - just awesome!' ,
-  //       'https://pngimg.com/uploads/schnitzel/schnitzel_PNG9.png',
-  //       [
-  //           new Ingredient('Meat', 1),
-  //           new Ingredient('French Fries', 20)
-  //       ]),
-  //       new Recipe('Big Fat Burger', 'What else you need to say?',
-  //       'http://static.businessinsider.com/image/53601a026bb3f7e3512eb8a7-750.jpg',
-  //        [
-  //            new Ingredient('Buns', 2),
-  //            new Ingredient('Meat', 1)
-  //        ])
-  //     ];
-
   private recipes: Recipe[] = [];
   private deleted: boolean;
-      private itemIdForDelete: number;
+  private itemIdForDelete: number;
+  private subscription: Subscription;
 
-      setRecipes(recipes: Recipe[]) {
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  getRecipes() {
+    this.subscription = this.dataProvider.getRecipes()
+      .subscribe((recipes: Recipe[]) => {
         this.recipes = recipes;
-        this.recipesChanged.next(this.recipes.slice());
-      }
+      });
 
-      getRecipes() {
-          // return this.recipes.slice();
-          this.recipes = this.dataProvider.getRecipes();
-          this.recipesChanged.next(this.recipes);
-          return this.recipes;
-      }
+    this.recipesChanged.next(this.recipes);
+    return this.recipes;
+  }
 
-      getRecipe(index: number) {
-          return this.recipes[index];
-      }
+  getRecipe(index: number) {
+      return this.recipes[index];
+  }
 
-      addIngrediantsToShoppingList(ingrediants: Ingredient[]) {
-        this.slService.addIngredients(ingrediants);
-      }
+  addIngrediantsToShoppingList(ingrediants: Ingredient[]) {
+    this.slService.addIngredients(ingrediants);
+  }
 
-      addRecipe(recipe: Recipe) {
-        this.recipes.push(recipe);
-        this.recipesChanged.next(this.recipes.slice());
-      }
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
-      updateRecipe(index: number, newRecipe: Recipe) {
-        this.recipes[index] = newRecipe;
-        this.recipesChanged.next(this.recipes.slice());
-      }
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
-      async storeRecipe() {
-        await this.dataProvider.storeRecipes(this.recipes);
-      }
+  storeRecipe() {
+    this.dataProvider.storeRecipes(this.recipes);
+  }
 
-      async deleteRecipe(index: number) {
-        this.itemIdForDelete = this.getRecipe(index).id;
+  deleteRecipe(index: number) {
+    this.itemIdForDelete = this.getRecipe(index).id;
 
-        this.deleted = await this.dataProvider.deleteRecipe(this.itemIdForDelete);
-        if (this.deleted) {
-          this.recipes.splice(index, 1);
-          this.recipesChanged.next(this.recipes.slice());
-        }
-      }
+    this.dataProvider.deleteRecipe(this.itemIdForDelete)
+      .subscribe((del: boolean) => {
+        this.deleted = del;
+      });
 
+    if (this.deleted) {
+      this.recipes.splice(index, 1);
+      this.recipesChanged.next(this.recipes.slice());
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
 }
--- Frontend/src/app/shared/database-data.service.ts
+++ Frontend/src/app/shared/database-data.service.ts
@@ -3,6 +3,7 @@ import 'rxjs/add/operator/map';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Recipe } from '../recipes/recipe';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class DatabaseDataService {
@@ -19,30 +20,19 @@ public url = 'http://localhost:5000/api/recipe';
 private recipe: Recipe[];
 private isDeleted: boolean;
 
-async storeRecipes(recipe: Recipe[]) {
-  await this.http.post(this.url + '/postRecipe', recipe, this.HttpOptions)
-  .toPromise()
-      .then((response: Response) =>
+storeRecipes(recipe: Recipe[]) {
+  this.http.post(this.url + '/postRecipe', recipe, this.HttpOptions)
+  .subscribe((response: Response) =>
       console.log(response)
       );
 }
 
-getRecipes() {
-  this.http.get<Recipe[]>(this.url + '/recipes', this.HttpOptions)
-  .subscribe((recipes: Recipe[]) =>
-    this.recipe = recipes
-  );
-  return this.recipe;
+getRecipes(): Observable<Recipe[]> {
+  return this.http.get<Recipe[]>(this.url + '/recipes', this.HttpOptions);
 }
 
-async deleteRecipe(index: number) {
-  this.isDeleted = await this.http.delete<boolean>(this.url + '/delete/' + index, this.HttpOptions).toPromise();
-      // .toPromise()
-      //   .then((deleted: boolean) =>
-      //     this.isDeleted = deleted
-      //   );
-
-  return this.isDeleted;
+deleteRecipe(index: number): Observable<boolean> {
+  return this.http.delete<boolean>(this.url + '/delete/' + index, this.HttpOptions);
 }
- 
+
 }
